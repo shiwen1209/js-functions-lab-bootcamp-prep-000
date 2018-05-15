b84390408413bfff5301f4cf486cac751b9a7d48
Wendy She <whshiwen@gmail.com>
2018-05-15 19:30:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // write your code below!
-function happyHolidays(){console.log(`Happy holidays`)}
+function happyHolidays(){return("Happy holidays!")}
+function happyHolidaysTo(name){return(`Happy holidays, ${name}!`)}
+function happyHolidayTo(holiday,name){return(`Happy ${holiday}, ${name}!`)}
+function holidayCountdown(days, holiday){return(`It's ${days} days until ${holiday}!`)}
